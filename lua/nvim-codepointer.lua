d4d960fee7d1ce31c3de6dc0d0ce747e5b4974af
KanielDasper <dkdanielkasper@gmail.com>
2026-07-01 20:57:07 +0200

    feat: more logic to plugin, add doc/ and tests/

--- lua/nvim-codepointer.lua
+++ lua/nvim-codepointer.lua
@@ -1,17 +1,17 @@
 local M = {}
 
-function M.codepointer()
+function M.codepointer(opts)
 	local full_path = vim.fn.expand("%:p")
+	local project_root = vim.fn.finddir(".git", full_path)
 	vim.print(full_path)
 end
 
-function M.setup()
+function M.setup(opts)
 	opts = opts or {}
 
 	vim.api.nvim_create_user_command("CodePointer", M.codepointer, {})
 
 	local keymap = opts.keymap() or "<leader>cp"
-
 	vim.keymap.set("n", keymap, M.codepointer, {
 		desc = "Create a codepointer",
 		silent = true,
--- plugin/nvim-codepointer.lua
+++ plugin/nvim-codepointer.lua
@@ -1,3 +1,3 @@
-vim.api.nvim_create_user_command("CodepointerInfo", function()
+vim.api.nvim_create_user_command("CodePointerInfo", function()
 	vim.notify("Plugin is loaded!", vim.log.levels.WARN)
 end, { desc = "Show info about codepointer plugin status" })
