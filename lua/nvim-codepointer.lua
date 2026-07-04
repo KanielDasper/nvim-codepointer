3b00bc6180e2a7c36b8b1430387a170c04b20159
KanielDasper <dkdanielkasper@gmail.com>
2026-07-04 11:56:52 +0200

    feat: first working state

--- lua/nvim-codepointer.lua
+++ lua/nvim-codepointer.lua
@@ -1,19 +1,43 @@
 local M = {}
 
-function M.codepointer()
-	local full_path = vim.fn.expand("%:p")
-	local project_root = vim.fn.finddir(".git", full_path)
-	vim.print(full_path .. project_root)
+function M.get_codepointer()
+	local function get_project_file_path()
+		local current_file = vim.fs.basename(vim.api.nvim_buf_get_name(0))
+		local codepointer_path_tbl = { current_file }
+
+		for dir in vim.fs.parents(vim.api.nvim_buf_get_name(0)) do
+			table.insert(codepointer_path_tbl, 1, "/")
+			table.insert(codepointer_path_tbl, 1, vim.fs.basename(dir))
+
+			if vim.fn.isdirectory(dir .. "/.git") == 1 then
+				break
+			end
+		end
+		return table.concat(codepointer_path_tbl)
+	end
+
+	local function get_linenum_pos()
+		local cursor_tbl = vim.api.nvim_win_get_cursor(0)
+		return cursor_tbl[1] -- { line_num, col_num }
+	end
+
+	local project_file_path = get_project_file_path()
+	local line_number = get_linenum_pos()
+	return project_file_path .. ":" .. line_number
+end
+
+function M.print_codepointer()
+	vim.print(M.get_codepointer())
 end
 
 function M.setup(opts)
 	opts = opts or {}
 
-	vim.api.nvim_create_user_command("CodePointer", M.codepointer, {})
+	vim.api.nvim_create_user_command("CodePointer", M.print_codepointer, {})
 
 	local keymap = opts.keymap or "<leader>cp"
-	vim.keymap.set("n", keymap, M.codepointer, {
-		desc = "Create a codepointer for current file and line",
+	vim.keymap.set("n", keymap, M.get_codepointer, {
+		desc = "Create a [C]ode[P]ointer for current file and line",
 		silent = true,
 	})
 end
